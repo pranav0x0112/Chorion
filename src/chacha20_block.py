6d14772d7db44233c6c0fe50f87b542e999498d3
prxnav2005 <pranavmofficial2005@gmail.com>
2025-03-07 23:08:56 +0530

    feat: chacha20 block added

---
+++ src/chacha20_block.py
@@ -0,0 +1,9 @@
+from rounds import double_round
+
+def block(state):
+    working_state = state.copy()
+    for _ in range(10):
+        working_state = double_round(working_state)
+    
+    return[(x+y) & 0xFFFFFFFF for x,y in zip(state, working_state)]
+
