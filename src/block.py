6ae2d80e23f03704c70f5467beec35964e3b5169
prxnav2005 <pranavmofficial2005@gmail.com>
2025-03-08 22:14:52 +0530

    fix: correct function name

--- src/block.py
+++ src/block.py
@@ -1,9 +1,8 @@
 from rounds import double_round
 
-def block(state):
+def chacha20_block(state):
     working_state = state.copy()
     for _ in range(10):
         working_state = double_round(working_state)
     
     return[(x+y) & 0xFFFFFFFF for x,y in zip(state, working_state)]
-
--- src/state.py
+++ src/state.py
@@ -1,8 +1,7 @@
-def state(key: bytes, counter: int, nonce: bytes):
+def chacha20_state(key: bytes, counter: int, nonce: bytes):
     constants = [0x61707865, 0x3320646e, 0x79622d32, 0x6b206574]
 
     key_words = [int.from_bytes(key[i:i+4], 'little') for i in range(0,32,4)]
     nonce_words = [int.from_bytes(nonce[i:i+4], 'little') for i in range(0,12,4)]
 
     return constants + key_words + [counter] + nonce_words
-
