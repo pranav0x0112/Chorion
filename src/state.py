6dc352b36bf1967d3ad2427110e447da0669c5d4
prxnav2005 <pranavmofficial2005@gmail.com>
2025-03-08 21:55:54 +0530

    feat: chacha20 state added

---
+++ src/state.py
@@ -0,0 +1,8 @@
+def state(key: bytes, counter: int, nonce: bytes):
+    constants = [0x61707865, 0x3320646e, 0x79622d32, 0x6b206574]
+
+    key_words = [int.from_bytes(key[i:i+4], 'little') for i in range(0,32,4)]
+    nonce_words = [int.from_bytes(nonce[i:i+4], 'little') for i in range(0,12,4)]
+
+    return constants + key_words + [counter] + nonce_words
+
