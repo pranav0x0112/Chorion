c1e7cd0ff25d2b74fc961e14813dc2e182a46c5e
prxnav2005 <pranavmofficial2005@gmail.com>
2025-03-15 20:09:28 +0530

    fix: module instantiation error

--- hdl/src/block.sv
+++ hdl/src/block.sv
@@ -1,57 +1,29 @@
-// Code your design here
 module chacha20_block(input logic clk, reset, start, input logic [31:0] state_in [0:15], output logic [31:0] keystream [0:15], output logic done);
-  
-  logic [31:0] state_reg [0:15];
-  logic [4:0] round_counter;
+
+  logic [31:0] state_reg [0:15], qr_out [0:15];  
+  logic [4:0] round_counter;  
   logic processing;
-  
-  always_ff@(posedge clk or posedge reset)
-    begin
-      if(reset)
-        begin
-          round_counter <= 0;
-          processing <= 0;
-          done <= 0;
-        end
-      else if(start)
-        begin
-          state_reg <= state_in;
-          round_counter <= 0;
-          processing <= 1;
-          done <= 0;
-        end
-      else if(processing)
-        begin
-          if(round_counter < 20)
-            begin
-              if(round_counter % 2 == 0)
-                begin
-                  quarter_round qr0 (state_reg[0], state_reg[4], state_reg[8], state_reg[12], state_reg[0], state_reg[4], state_reg[8], state_reg[12]);
-                  quarter_round qr1 (state_reg[1], state_reg[5], state_reg[9], state_reg[13], state_reg[1], state_reg[5], state_reg[9], state_reg[13]);
-                  quarter_round qr2 (state_reg[2], state_reg[6], state_reg[10], state_reg[14], state_reg[2], state_reg[6], state_reg[10], state_reg[14]);
-                  quarter_round qr3 (state_reg[3], state_reg[7], state_reg[11], state_reg[15], state_reg[3], state_reg[7], state_reg[11], state_reg[15]);
-                end
-              else
-                begin
-                  quarter_round qr4 (state_reg[0], state_reg[5], state_reg[10], state_reg[15], state_reg[0], state_reg[5], state_reg[10], state_reg[15]);
-                  quarter_round qr5 (state_reg[1], state_reg[6], state_reg[11], state_reg[12], state_reg[1], state_reg[6], state_reg[11], state_reg[12]);
-                  quarter_round qr6 (state_reg[2], state_reg[7], state_reg[8], state_reg[13], state_reg[2], state_reg[7], state_reg[8], state_reg[13]);
-                  quarter_round qr7 (state_reg[3], state_reg[4], state_reg[9], state_reg[14], state_reg[3], state_reg[4], state_reg[9], state_reg[14]);
-                end
-              round_counter <= round_counter + 1;
-            end
-          else 
-            begin
-              for(int i = 0; i < 16; i++)
-                begin
-                  keystream[i] = state_in[i] + state_reg[i];
-                end
-              done <= 1;
-              processing <= 0;
-            end
-        end
+
+  generate
+    genvar i;
+    for (i = 0; i < 4; i++) begin : qr_even
+      quarter_round qr_even_inst(state_reg[i], state_reg[i+4], state_reg[i+8], state_reg[i+12], qr_out[i], qr_out[i+4], qr_out[i+8], qr_out[i+12]);
     end
+  endgenerate
+
+  always_ff @(posedge clk or posedge reset) begin
+    if (reset) begin
+      round_counter <= 0; processing <= 0; done <= 0;
+    end else if (start) begin
+      state_reg <= state_in; round_counter <= 0; processing <= 1; done <= 0;
+    end else if (processing) begin
+      if (round_counter < 20) begin
+        if (round_counter % 2 == 0) state_reg <= qr_out;
+        round_counter <= round_counter + 1;
+      end else begin
+        for (int i = 0; i < 16; i++) keystream[i] = state_in[i] + state_reg[i];
+        done <= 1; processing <= 0;
+      end
+    end
+  end
 endmodule
-
-
-                  
--- hdl/src/quarter_round.sv
+++ hdl/src/quarter_round.sv
@@ -1,18 +1,17 @@
-// Code your design here
 module quarter_round(input logic [31:0] a,b,c,d, output logic [31:0] a_out, b_out, c_out, d_out);
   
-  logic [31:0] t0,t1,t2,t3;
+  logic [31:0] t0,t1,t2,t3,d_temp, b_temp;
   
   always_comb
     begin
-      t0 = a + b; d ^= t0; d = {d[15:0], d[31:16]};  // <<< 16
-      t1 = c + d; b ^= t1; b = {b[11:0], b[31:12]};  // <<< 12
-      t2 = t0 + b; d ^= t2; d = {d[23:0], d[31:24]}; // <<< 8
-      t3 = t1 + d; b ^= t3; b = {b[6:0], b[31:7]};   // <<< 7
+      t0 = a + b; d_temp = d ^ t0; d_temp = {d_temp[15:0], d_temp[31:16]};  // <<< 16
+      t1 = c + d_temp; b_temp = b ^ t1; b_temp = {b_temp[11:0], b_temp[31:12]};  // <<< 12
+      t2 = t0 + b_temp; d_temp = d_temp ^ t2; d_temp = {d_temp[23:0], d_temp[31:24]}; // <<< 8
+      t3 = t1 + d_temp; b_temp = b_temp ^ t3; b_temp = {b_temp[6:0], b_temp[31:7]};   // <<< 7
       
       a_out = t2;
-      b_out = b;
+      b_out = b_temp;
       c_out = t3;
-      d_out = d;
+      d_out = d_temp;
     end
 endmodule
